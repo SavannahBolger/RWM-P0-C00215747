67a2c308ce55552891d483577461174f44b9a3a2
SavannahBolger <C00215747@itcarlow.ie>
2019-09-25 11:22:45 +0100

    add lab

---
+++ RWM-P0-C00215747/Assets/Scripts/CubeFilter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFilter
+{
+    
+    public int cube(int i)
+    {
+        if(i % 3 == 0 && i % 5 == 0)
+        {
+            return i = 0;
+        }
+        else if (i % 3 == 0 && i % 5 != 0)
+        {
+            return i = 3;
+        }
+        else if (i % 3 != 0 && i % 5 == 0)
+        {
+            return i = 5;
+        }
+    }
+
+    public int[] cubes(int[] i)
+    {
+
+        int[] result;
+        for(int x = 0; x < i.Length; x++)
+        {
+            result.
+        }
+    }
+}
---
+++ RWM-P0-C00215747/Assets/UTech/MG-Platformer/ModAssets/ModResources/Scripts/Platformer/Tests/CubeFilterTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFilterTest
+{
+    public void FilterTest()
+    {
+        int[] input = { 4, 3, 6, 30, 10, 12 };
+        int[] output = CubeFilter.cubes(input);
+        int[] result = { 4, 3, 3, 0, 5, 3 };
+
+        CubeFilterTest.Equals(output, result);
+    }
+}
